c1396b0fa730c9167b54cbb4f10b29add47e51ed
toshitk <takahashi.t66@gmail.com>
2023-09-04 10:18:35 +0900

    chore: specify user base

--- migration/reset_database.py
+++ migration/reset_database.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 
 from api.infrastructrure.database.model.base import Base
+from api.infrastructrure.database.model.user import Base as user
 
 DB_URL = "mysql+pymysql://root@db:3306/eucalyptus_local?charset=utf8"
 engine = create_engine(DB_URL, echo=True)
@@ -8,7 +9,7 @@ engine = create_engine(DB_URL, echo=True)
 
 def reset_database():
     Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
+    user.metadata.create_all(bind=engine)
 
 
 if __name__ == "__main__":
