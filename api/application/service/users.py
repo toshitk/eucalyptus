7bdd783a6c83ac3ca7bd71b2602d5455ee873f45
toshitk <takahashi.t66@gmail.com>
2023-09-04 11:31:57 +0900

    feat: implement service

--- api/application/service/users.py
+++ api/application/service/users.py
@@ -1,6 +1,9 @@
+from sqlalchemy.orm import Session
+
 from api.infrastructrure.database.model.user import User
+from api.infrastructrure.database.repository.users import UsersRepository
 
 
 class UsersService:
-    def list_by_id(self, id: int) -> list[User]:
-        pass
+    def list_by_id(self, session: Session, id: str) -> list[User]:
+        return UsersRepository.list_by_id(session=session, id=id)
