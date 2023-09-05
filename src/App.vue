a2e172aad36b3a5a74030af587715c0de4dab447
toshitk <57493449+toshitk@users.noreply.github.com>
2023-09-05 21:48:30 +0900

    feat: vue create

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>API Response:</h1>
+    <p>{{ response }}</p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      response: null,
+    };
+  },
+  async created() {
+    try {
+      const res = await axios.get('http://localhost:8000/users/1');
+      this.response = res.data;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
+</script>
