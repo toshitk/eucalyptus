d4fff12e2e576d575640a6107a0ead0018f018fa
toshitk <57493449+toshitk@users.noreply.github.com>
2023-09-06 20:51:58 +0900

    chore: vite, vuetify

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,26 +1,33 @@
 <template>
   <div>
-    <h1>API Response:</h1>
-    <p>{{ response }}</p>
+    <button @click="fetchData">Fetch Data</button>
+    <pre>{{ data }}</pre>
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import { ref, defineComponent } from 'vue';
 import axios from 'axios';
 
-export default {
-  data() {
-    return {
-      response: null,
+export default defineComponent({
+  name: 'FetchDataComponent',
+  setup() {
+    const data = ref<any>(null);
+
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8000/users/1');
+        data.value = response.data;
+      } catch (error) {
+        console.error('An error occurred:', error);
+        data.value = 'An error occurred';
+      }
     };
-  },
-  async created() {
-    try {
-      const res = await axios.get('http://localhost:8000/users/1');
-      this.response = res.data;
-    } catch (error) {
-      console.error(error);
-    }
-  },
-};
+
+    return {
+      data,
+      fetchData
+    };
+  }
+});
 </script>
--- web/vue.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    client: {
-      webSocketURL: 'ws://0.0.0.0:8080/ws'
-    }
-  }
-})
