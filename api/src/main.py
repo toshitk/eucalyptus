4ccb96dc3cfe3c622e35ad745299dbe45ca919e1
toshitk <57493449+toshitk@users.noreply.github.com>
2023-09-12 15:58:35 +0900

    chore: rename variable

--- api/src/main.py
+++ api/src/main.py
@@ -3,13 +3,13 @@ from fastapi.middleware.cors import CORSMiddleware
 from src.presentation.controller import users_router
 
 app = FastAPI()
-origins = [
+allow_origins = [
     "http://localhost:3000",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=allow_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
