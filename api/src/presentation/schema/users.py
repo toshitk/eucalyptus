f411f3bc74623d2b5dafcf968c7baf1f20e68969
toshitk <57493449+toshitk@users.noreply.github.com>
2023-09-13 21:55:55 +0900

    chore: change example

--- api/src/presentation/schema/users.py
+++ api/src/presentation/schema/users.py
@@ -3,8 +3,8 @@ from pydantic import BaseModel, Field
 
 class ResponseModel(BaseModel):
     id: int = Field(..., example=1)
-    name: str = Field(..., example="Alice")
-    email: str = Field(..., example="alice@example.com")
+    name: str = Field(..., example="John Smith")
+    email: str = Field(..., example="sample@example.com")
 
     class Config:
         orm_mode = True
