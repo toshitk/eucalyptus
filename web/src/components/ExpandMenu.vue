45cd4bd54b9f4578c4d2b6ab220df77917be344d
toshitk <57493449+toshitk@users.noreply.github.com>
2023-09-13 17:17:35 +0900

    chore: remove comments

--- web/src/components/ExpandMenu.vue
+++ web/src/components/ExpandMenu.vue
@@ -1,24 +1,22 @@
 <template>
-  <!-- <v-card> -->
-    <v-layout>
-      <v-navigation-drawer class="header-margin" expand-on-hover rail :model-value="drawer" @update:model-value="$emit('update:drawer', $event)">
-        <v-list>
-          <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg" title="Sandra Adams"
-            subtitle="sandra_a88@gmailcom"></v-list-item>
-        </v-list>
+  <v-layout>
+    <v-navigation-drawer class="header-margin" expand-on-hover rail :model-value="drawer" @update:model-value="$emit('update:drawer', $event)">
+      <v-list>
+        <v-list-item prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg" title="Sandra Adams"
+          subtitle="sandra_a88@gmailcom"></v-list-item>
+      </v-list>
 
-        <v-divider></v-divider>
+      <v-divider></v-divider>
 
-        <v-list density="compact" nav>
-          <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
-          <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
-          <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+      <v-list density="compact" nav>
+        <v-list-item prepend-icon="mdi-folder" title="My Files" value="myfiles"></v-list-item>
+        <v-list-item prepend-icon="mdi-account-multiple" title="Shared with me" value="shared"></v-list-item>
+        <v-list-item prepend-icon="mdi-star" title="Starred" value="starred"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
-      <v-main></v-main>
-    </v-layout>
-  <!-- </v-card> -->
+    <v-main></v-main>
+  </v-layout>
 </template>
 
 <script lang="ts">
