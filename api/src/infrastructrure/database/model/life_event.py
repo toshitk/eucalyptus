866f6d2abb5c247555fb6fe87f20a7622c32d553
toshitk <takahashi.t66@gmail.com>
2023-09-14 17:03:15 +0900

    feat: life event model

---
+++ api/src/infrastructrure/database/model/life_event.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+
+from sqlalchemy import DATETIME, VARCHAR, Column, Integer
+
+from .base import Base
+
+
+class LifeEvent(Base):
+    __tablename__ = "life_events"
+
+    id = Column(Integer, primary_key=True)
+    plan_id = Column(Integer, nullable=False)
+    name = Column(VARCHAR(255), nullable=False)
+    year = Column(Integer, nullable=False)
+    month = Column(Integer, nullable=False)
+    deleted_at = Column(DATETIME, default=None)
+    created_at = Column(DATETIME, default=datetime.now, nullable=False)
+    updated_at = Column(
+        DATETIME, default=datetime.now, nullable=False, onupdate=datetime.now
+    )
+
+    def __init__(
+        self,
+        plan_id: int,
+        name: str,
+        year: int,
+        month: int,
+        deleted_at: datetime | None = None,
+        created_at: datetime | None = None,
+        updated_at: datetime | None = None,
+        id: int | None = None,
+    ):
+        self.id = id
+        self.plan_id = plan_id
+        self.name = name
+        self.year = year
+        self.month = month
+        self.deleted_at = deleted_at
+        self.created_at = created_at if created_at else datetime.now()
+        self.updated_at = updated_at if updated_at else datetime.now()
+
+    def __str__(self):
+        return f"id: {self.id}, \
+plan_id: {self.plan_id}, \
+name: {self.name}, \
+year: {self.year}, \
+month: {self.month}, \
+deleted_at: {self.deleted_at}, \
+created_at: {self.created_at}, \
+updated_at: {self.updated_at}"
