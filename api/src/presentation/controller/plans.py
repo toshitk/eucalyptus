84b99dc777d50614d22834063768969e70a04044
toshitk <takahashi.t66@gmail.com>
2023-09-14 14:06:29 +0900

    fix: response model

--- api/src/presentation/controller/plans.py
+++ api/src/presentation/controller/plans.py
@@ -8,6 +8,6 @@ router = APIRouter(prefix="/plans", tags=["plans"])
 
 class PlansController:
     @staticmethod
-    @router.get("/{user_id}", response_model=ResponseModel)
+    @router.get("/{user_id}", response_model=list[ResponseModel])
     async def get(user_id: int, session=Depends(create_session)):
         return await PlansService.list_my_plans(session=session, user_id=user_id)
