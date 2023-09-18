47b0cee0647daa7a9993b31bc39c3d7957a97292
toshitk <57493449+toshitk@users.noreply.github.com>
2023-09-18 09:42:16 +0900

    test: Update test_users.py

--- api/test/presentation/controller/test_users.py
+++ api/test/presentation/controller/test_users.py
@@ -1,2 +1,32 @@
+from unittest.mock import patch
+
+from fastapi.testclient import TestClient
+from pytest import fixture
+from src.infrastructrure.database.connection import create_session
+from src.infrastructrure.database.model.user import User
+from src.main import app
+
+_service_class = "src.application.service.users.UsersService"
+
+
+@fixture
+def _fixture():
+    app.dependency_overrides[create_session] = lambda: "dummy session"
+    client = TestClient(app=app, base_url="http://test")
+    return client
+
+
 class TestUsersController:
-    pass
+    @patch(f"{_service_class}.find_by_id")
+    def test_get(self, find_by_id, _fixture):
+        client = _fixture
+
+        find_by_id.return_value = User(id=1, name="John", email="@example.com")
+
+        expected = {"id": 1, "name": "John", "email": "@example.com"}
+
+        response = client.get("/users/1")
+
+        assert response.status_code == 200
+        assert response.json() == expected
+        find_by_id.assert_called_once_with(session="dummy session", id=1)
