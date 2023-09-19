800918a58d0edbed114cd02bd7885c6b2c61596f
toshitk <takahashi.t66@gmail.com>
2023-09-19 18:51:42 +0900

    chore: change alembic file template

--- api/migration/versions/2023_09_19_1849-ff9881bf542d_create_table_life_events.py
+++ api/migration/versions/2023_09_19_1849-ff9881bf542d_create_table_life_events.py
@@ -1,8 +1,8 @@
-"""create table(plans)
+"""create table(life_events)
 
-Revision ID: 6b62a03b755d
-Revises: 71ae9af4bed2
-Create Date: 2023-09-19 11:37:59.940088
+Revision ID: ff9881bf542d
+Revises: f21db4b4c97f
+Create Date: 2023-09-19 18:49:37.211023
 
 """
 from datetime import datetime
@@ -13,18 +13,20 @@ from alembic import op
 from sqlalchemy import DATETIME, VARCHAR, Column, Integer
 
 # revision identifiers, used by Alembic.
-revision: str = "6b62a03b755d"
-down_revision: Union[str, None] = "71ae9af4bed2"
+revision: str = "ff9881bf542d"
+down_revision: Union[str, None] = "f21db4b4c97f"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
     op.create_table(
-        "plans",
+        "life_events",
         Column("id", Integer, primary_key=True),
-        Column("user_id", Integer, nullable=False),
+        Column("plan_id", Integer, nullable=False),
         Column("name", VARCHAR(255), nullable=False),
+        Column("year", Integer, nullable=False),
+        Column("month", Integer, nullable=False),
         Column("deleted_at", DATETIME, default=None),
         Column("created_at", DATETIME, default=datetime.now, nullable=False),
         Column(
@@ -39,13 +41,15 @@ def upgrade() -> None:
     op.execute(
         sa.text(
             """
-            INSERT INTO plans (id, user_id, name, created_at, updated_at)
-            VALUES (:id, :user_id, :name, :created_at, :updated_at)
+            INSERT INTO life_events (id, plan_id, name, year, month, created_at, updated_at)
+            VALUES (:id, :plan_id, :name, :year, :month, :created_at, :updated_at)
         """
         ).bindparams(
             sa.bindparam("id", 1),
-            sa.bindparam("user_id", 1),
-            sa.bindparam("name", "Sample Plan"),
+            sa.bindparam("plan_id", 1),
+            sa.bindparam("name", "Marriage"),
+            sa.bindparam("year", 2000),
+            sa.bindparam("month", 1),
             sa.bindparam("created_at", "2000-01-01"),
             sa.bindparam("updated_at", "2000-01-01"),
         )
@@ -53,4 +57,4 @@ def upgrade() -> None:
 
 
 def downgrade() -> None:
-    op.drop_table("plans")
+    op.drop_table("life_events")
