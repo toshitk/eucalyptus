4b88cbaf500898e589a8d53ab9cc1c3eba37e7de
toshitk <takahashi.t66@gmail.com>
2023-09-20 16:32:32 +0900

    feat: migration

---
+++ api/migration/versions/2023_09_20_1447-bc8b12fcd7ab_alter_table_users.py
@@ -0,0 +1,42 @@
+"""alter table(users)
+
+Revision ID: bc8b12fcd7ab
+Revises: ff9881bf542d
+Create Date: 2023-09-20 14:47:43.793638
+
+"""
+from datetime import datetime
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+from sqlalchemy import DATETIME, VARCHAR, Column, Integer
+
+# revision identifiers, used by Alembic.
+revision: str = "bc8b12fcd7ab"
+down_revision: Union[str, None] = "ff9881bf542d"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.add_column("users", sa.Column("auth0_id", sa.VARCHAR(255)))
+    op.execute(
+        sa.text(
+            """
+            UPDATE users SET auth0_id = 'auth0|650a890c07c072bfdfa8a78a'
+            WHERE id = 1
+        """
+        )
+    )
+    op.alter_column(
+        "users",
+        "auth0_id",
+        existing_type=sa.Integer,
+        type_=sa.VARCHAR(255),
+        nullable=False,
+    )
+
+
+def downgrade() -> None:
+    op.drop_column("users", "auth0_id")
