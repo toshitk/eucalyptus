81316e5fedb005a28e6808b059af45127bf7a91e
toshitk <takahashi.t66@gmail.com>
2023-09-20 10:28:20 +0900

    chore: remove comment

--- api/src/infrastructrure/auth/auth.py
+++ api/src/infrastructrure/auth/auth.py
@@ -14,7 +14,13 @@ def auth():
         async def wrapper(*args, **kwargs):
             request = kwargs.get("request")
             authorization = request.headers.get("Authorization")
-            # _analyze_token(authorization=authorization)
+            if authorization:
+                _analyze_token(authorization=authorization)
+            else:
+                raise HTTPException(
+                    status_code=401, detail="Authorization header is required"
+                )
+
             return await func(*args, **kwargs)
 
         return wrapper
@@ -22,13 +28,30 @@ def auth():
     return decorator
 
 
-def get_jwks():
-    response = requests.get(f"https://{auth0_domain}/.well-known/jwks.json")
-    return response.json()
+def _analyze_token(authorization: str) -> None:
+    token = _parse_authorization_header(authorization=authorization)
+    rsa_key = _get_public_key(token=token)
+
+    if not rsa_key:
+        raise HTTPException(status_code=401, detail="Unable to find appropriate key")
+
+    decoded_jwt = _decode_jwt_token(token=token, key=rsa_key)
 
 
-def get_public_key(token: str):
-    jwks = get_jwks()
+def _parse_authorization_header(authorization):
+    splited_values = authorization.split()
+
+    if splited_values[0].lower() != "bearer":
+        raise HTTPException(status_code=401, detail="Invalid Authorization header")
+
+    if len(splited_values) != 2:
+        raise HTTPException(status_code=401, detail="Invalid Authorization header")
+
+    return splited_values[1]
+
+
+def _get_public_key(token: str):
+    jwks = _get_jwks()
     unverified_header = jwt.get_unverified_header(token)
     rsa_key = {}
     for key in jwks["keys"]:
@@ -43,24 +66,15 @@ def get_public_key(token: str):
     return rsa_key
 
 
-def _analyze_token(authorization: str) -> None:
-    token = _parse_authorization_header(authorization=authorization)
-    print(f"token: {token}")
-
-    rsa_key = get_public_key(token=token)
-    print(f"rsa_key: {rsa_key}")
-
-    if not rsa_key:
-        raise HTTPException(status_code=401, detail="Unable to find appropriate key")
-
-    decoded_jwt = _decode_jwt_token(token=token, key=rsa_key)
-    print(f"decoded_jwt: {decoded_jwt}")
+def _get_jwks():
+    response = requests.get(f"https://{auth0_domain}/.well-known/jwks.json")
+    return response.json()
 
 
 def _decode_jwt_token(token: str, key: str) -> dict:
     try:
         payload = jwt.decode(
-            jwt=token,
+            token,
             key=key,
             algorithms=["RS256"],
             issuer="https://dev-e3ybvpddsjl3sbd7.us.auth0.com/",
@@ -72,15 +86,3 @@ def _decode_jwt_token(token: str, key: str) -> dict:
         raise HTTPException(status_code=401, detail="Error decoding token")
 
     return payload
-
-
-def _parse_authorization_header(authorization):
-    splited_values = authorization.split()
-
-    if splited_values[0].lower() != "bearer":
-        raise HTTPException(status_code=401, detail="Invalid Authorization header")
-
-    if len(splited_values) != 2:
-        raise HTTPException(status_code=401, detail="Invalid Authorization header")
-
-    return splited_values[1]
