e63ee44cdc5ed32eab03c082e0b10fc50c9291ea
toshitk <takahashi.t66@gmail.com>
2023-09-21 17:39:44 +0900

    test: users service

--- api/test/application/service/test_users.py
+++ api/test/application/service/test_users.py
@@ -12,6 +12,26 @@ class TestUserService:
     @patch(f"{_repo}.find_by_id")
     async def test_find_by_id(self, find_by_id):
         find_by_id.new_callable = AsyncMock
-        await UsersService.find_by_id(session="dummy", id=1)
+        find_by_id.return_value = User(
+            id=1, auth0_id="dummy id", name="john", email="test@exampl.com"
+        )
+        result = await UsersService.find_by_id(session="dummy", id=1)
 
         find_by_id.assert_called_once_with(session="dummy", id=1)
+        assert isinstance(result, User)
+
+    @pytest.mark.asyncio
+    @patch(f"{_repo}.insert")
+    async def test_create(self, insert):
+        insert.new_callable = AsyncMock
+        insert.return_value = User(
+            auth0_id="dummy id", name="john", email="test@exampl.com"
+        )
+        result = await UsersService.create(
+            session="dummy", auth0_id="dummy id", name="john", email="test@example.com"
+        )
+
+        insert.assert_called_once_with(
+            session="dummy", auth0_id="dummy id", name="john", email="test@example.com"
+        )
+        assert isinstance(result, User)
