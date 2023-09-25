b60fee53dc0791565f9a9c00a681742e6a4eee3b
toshitk <takahashi.t66@gmail.com>
2023-09-25 15:43:19 +0900

    feat: financial events model

---
+++ api/src/infrastructure/database/model/financial_events.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+
+from sqlalchemy import DATETIME, VARCHAR, Column, Integer
+
+from .base import Base
+
+
+class FinancialEvent(Base):
+    __tablename__ = "financial_events"
+
+    id = Column(Integer, primary_key=True)
+    plan_id = Column(Integer, nullable=False)
+    type = Column(Integer, nullable=False)  # 1: income, 2:saving, 3: expense
+    category = Column(VARCHAR(255))
+    name = Column(VARCHAR(255), nullable=False)
+    year = Column(Integer, nullable=False)
+    month = Column(Integer, nullable=False)
+    deleted_at = Column(DATETIME, default=None)
+    created_at = Column(DATETIME, default=datetime.now, nullable=False)
+    updated_at = Column(
+        DATETIME, default=datetime.now, nullable=False, onupdate=datetime.now
+    )
+
+    def __init__(
+        self,
+        plan_id: int,
+        type: int,
+        category: str,
+        name: str,
+        year: int,
+        month: int,
+        deleted_at: datetime | None = None,
+        created_at: datetime | None = None,
+        updated_at: datetime | None = None,
+        id: int | None = None,
+    ):
+        self.id = id
+        self.type = type
+        self.category = category
+        self.plan_id = plan_id
+        self.name = name
+        self.year = year
+        self.month = month
+        self.deleted_at = deleted_at
+        self.created_at = created_at if created_at else datetime.now()
+        self.updated_at = updated_at if updated_at else datetime.now()
+
+    def __str__(self):
+        return f"id: {self.id}, \
+plan_id: {self.plan_id}, \
+type: {self.type}, \
+category: {self.category}, \
+name: {self.name}, \
+year: {self.year}, \
+month: {self.month}, \
+deleted_at: {self.deleted_at}, \
+created_at: {self.created_at}, \
+updated_at: {self.updated_at}"
