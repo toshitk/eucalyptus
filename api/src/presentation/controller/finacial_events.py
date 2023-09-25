98cc725274acfc545242f45ab27750c203da96fe
toshitk <takahashi.t66@gmail.com>
2023-09-25 15:34:08 +0900

    feat: create model

--- api/src/presentation/controller/finacial_events.py
+++ api/src/presentation/controller/finacial_events.py
@@ -1,13 +1,14 @@
 from fastapi import APIRouter, Depends, Request
 from src.infrastructure.auth import auth
 from src.infrastructure.database.connection import create_session
+from src.presentation.schema.financial_events import ResponseModel
 
-router = APIRouter(prefix="/financial_events", tags=["financial_events"])
+router = APIRouter(prefix="/financialEvents", tags=["financialEvents"])
 
 
 class FinancialEventsController:
     @staticmethod
-    @router.get("/{plan_id}")
+    @router.get("/{plan_id}", response_model=list[ResponseModel])
     async def get(
         request: Request,
         plan_id: int,
---
+++ api/src/presentation/schema/financial_events.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel, Field
+
+
+class ResponseModel(BaseModel):
+    id: int = Field(...)
+    plan_id: int = Field(...)
+    type: int = Field(...)
+    category: str = Field(...)
+    name: str = Field(...)
+    year: int = Field(...)
+    month: int = Field(...)
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "id": 1,
+                "plan_id": 1,
+                "type": 1,
+                "category": "salaly",
+                "name": "monthly salary",
+                "year": 2000,
+                "month": 1,
+            }
+        }
+        from_attributes = True
