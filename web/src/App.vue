b55499b53b0f852a2d0983bf7f63af44a6c94870
toshitk <takahashi.t66@gmail.com>
2023-09-27 17:12:17 +0900

    feat: auth

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,31 +1,30 @@
 <template>
-  <div id="app">
-    <GlobalMenu @toggle-drawer="toggleDrawer" />
-    <ExpandMenu :drawer="drawer" @update:drawer="drawer = $event" />
-    <LineChart />
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
-<script lang="ts">
-import ExpandMenu from "./components/ExpandMenu.vue";
-import GlobalMenu from "./components/GlobalMenu.vue";
-import LineChart from "./components/LineChart.vue";
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  components: {
-    ExpandMenu,
-    GlobalMenu,
-    LineChart,
-  },
-  data() {
-    return {
-      drawer: true,
-    };
-  },
-  methods: {
-    toggleDrawer() {
-      this.drawer = !this.drawer;
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
     }
   }
 }
-</script>
+</style>
