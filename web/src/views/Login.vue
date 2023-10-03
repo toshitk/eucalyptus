4b65e15d047bb1334e8a4d9648c61e3c6b6e3fb8
toshitk <takahashi.t66@gmail.com>
2023-10-03 14:40:02 +0900

    chore: login view

---
+++ web/src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="login-button">
+    <button @click="login">Login</button>
+  </div>
+</template>
+
+<style>
+.login-button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
+
+<script>
+export default {
+  name: 'Login',
+  methods: {
+    // Log the user in
+    login() {
+      // console.log(this.$auth)
+      // console.log(this.$auth.loginWithRedirect)
+      this.$auth.loginWithRedirect();
+    },
+    // Log the user out
+    logout() {
+      this.$auth.logout({
+        returnTo: window.location.origin
+      });
+    }
+  }
+}
+</script>
