0c91de9e745858d8acc8476d2da0da504e45cee3
toshitk <takahashi.t66@gmail.com>
2023-10-05 11:26:03 +0900

    feat: call api

--- web/src/views/Login.vue
+++ web/src/views/Login.vue
@@ -5,6 +5,9 @@
   <div class="token-button">
     <button @click="doSomethingWithToken">getToken</button>
   </div>
+  <div class="token-button">
+    <button @click="fetchMe">getMe</button>
+  </div>
 </template>
 
 <style>
@@ -22,6 +25,7 @@
 
 <script>
 import { useAuth0 } from '@auth0/auth0-vue';
+import axios from 'axios';
 
 export default {
   setup() {
@@ -34,6 +38,15 @@ export default {
       doSomethingWithToken: async () => {
         const token = await getAccessTokenSilently();
         console.log(token)
+      },
+      fetchMe: async () => {
+        const token = await getAccessTokenSilently();
+        const response = await axios.get("http://localhost:8000/users/me", {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+        console.log(response.data)
       }
     };
   }
